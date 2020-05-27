5bc582372d3d9ddd1789e27f52bda6cdae868db0
stefano-b <bartoletti.stefano@gmail.com>
2020-05-27 14:16:43 +0200

    folder based routes

---
+++ module-2/pages/users/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    
+      
+      <h1 class="title">
+        The groups view
+      </h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
