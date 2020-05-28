86ba724cfdac75618c629f07fc665f00369e46d4
stefano-b <bartoletti.stefano@gmail.com>
2020-05-28 18:05:06 +0200

    dynamic routes

---
+++ module-2/pages/users/_id/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    
+      
+      <h1 class="title">
+        A single user with ID: {{ $route.params.id }}
+      </h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
