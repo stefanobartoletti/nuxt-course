7289af7bf98fd381f0fa3f682678af25e2620e75
stefano-b <bartoletti.stefano@gmail.com>
2020-05-29 16:40:43 +0200

    main pages

---
+++ module-3/pages/posts/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="posts-page">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.posts-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+}
+</style>
