caae67e6f814ec6cc8e0d52d5268a23d77d8140b
stefano-b <bartoletti.stefano@gmail.com>
2020-05-29 12:10:03 +0200

    nested route (nested content on page)

--- module-2/pages/users/index.vue
+++ module-2/pages/users/index.vue
@@ -1,31 +1,13 @@
 <template>
-  <div class="container">
-    <h1 class="title">The users view</h1>
-    <div>
-      <input type="text" v-model="userID">
-      <button @click="onLoadUser">Load User</button>
-    </div>
-  </div>
+  <p>Please enter an ID and click the button</p>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      userID: ''
-    }
-  },
-  methods: {
-    onLoadUser() {
-      this.$router.push('/users/' + this.userID)
-    }
-  },
+
 };
 </script>
 
 <style scoped>
-.container {
-  flex-direction: column;
-}
 
 </style>
