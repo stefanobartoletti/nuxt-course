77b9febb9172678a863fa60f26166140fa7e0150
stefano-b <bartoletti.stefano@gmail.com>
2020-06-03 19:37:37 +0200

    admin page

---
+++ module-3/pages/admin/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="admin-page">
+      <section class="new-post">
+          <button @click="$router.push('/admin/new-post')">Create new post</button>
+      </section>
+      <section class="existing-posts">
+          <h1>Existing posts</h1>
+          <post-list></post-list>
+      </section>
+  </div>
+</template>
+
+<script>
+import PostList from '@/components/Post/PostList'
+export default {
+components: {
+    PostList,
+},
+}
+</script>
+
+<style scoped>
+.admin-page {
+  padding: 20px;
+}
+
+.new-post {
+  text-align: center;
+  border-bottom: 2px solid #ccc;
+  padding-bottom: 10px;
+}
+
+.existing-posts h1 {
+  text-align: center;
+}
+</style>
---
+++ module-3/pages/admin/new-post/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="admin-new-post-page">
+    <section class="new-post-form">
+      <form @submit.prevent="onSave">
+        <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
+        <AppControlInput v-model="editedPost.title">Title</AppControlInput>
+        <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
+        <AppControlInput control-type="textarea" v-model="editedPost.content">Content</AppControlInput>
+
+        <AppButton type="submit">Save</AppButton>
+        <AppButton type="button" style="margin-left: 10px" btn-style="cancel" @click="onCancel">Cancel</AppButton>
+      </form>
+    </section>
+  </div>
+</template>
+
+<script>
+import AppControlInput from '@/components/UI/AppControlInput';
+import AppButton from '@/components/UI/AppButton';
+
+export default {
+    data() {
+        return {
+            editedPost: {
+                author: '',
+                title: '',
+                thumbnailLink: '',
+                content: '',
+            }
+        }
+    },
+    components: {
+        AppControlInput,
+        AppButton
+    },
+    methods: {
+        onSave() {
+            // Save the post
+            console.log(this.editedPost);
+        },
+        onCancel() {
+            // Navigate back
+            this.$router.push('/admin');
+        },
+    },
+};
+</script>
+
+<style scoped></style>
