565f07eb0749ebb7e88982391aedf10fe2281558
stefano-b <bartoletti.stefano@gmail.com>
2020-06-03 17:09:11 +0200

    header components

--- module-3/layouts/default.vue
+++ module-3/layouts/default.vue
@@ -1,13 +1,32 @@
 <template>
   <div>
+    <the-header @sidenavToggle="displaySidenav = !displaySidenav" />
+    <the-sidenav :show="displaySidenav" @close="displaySidenav = !displaySidenav" />
     <nuxt />
   </div>
 </template>
 
+<script>
+import TheHeader from "@/components/Navigation/TheHeader";
+import TheSidenav from "@/components/Navigation/TheSidenav";
+
+export default {
+  data() {
+    return {
+      displaySidenav: false,
+    }
+  },
+  components: {
+    TheHeader,
+    TheSidenav
+  }
+};
+</script>
+
 <style>
 html {
-  font-family: 'Open Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Open Sans", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -20,5 +39,4 @@ html {
 body {
   margin: 0;
 }
-
 </style>
