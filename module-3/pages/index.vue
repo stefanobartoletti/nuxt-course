d3bf770aeef23cb80d95fea67e97c717d4f94951
stefano-b <bartoletti.stefano@gmail.com>
2020-06-05 15:12:32 +0200

    dynamic data in PostList

--- module-3/pages/index.vue
+++ module-3/pages/index.vue
@@ -3,7 +3,7 @@
     <section class="intro">
       <h1>Get the latest tech news</h1>
     </section>
-    <post-list></post-list>
+    <post-list :posts="loadedPosts"></post-list>
   </div>
 </template>
 
@@ -11,6 +11,39 @@
 import PostList from "~/components/Post/PostList";
 
 export default {
+  data() {
+    return {
+      loadedPosts: [
+        {
+          id: "1",
+          title: "The Hobbit",
+          thumbnail:
+            "https://kbimages1-a.akamaihd.net/1db01021-c0ce-4afc-93fe-3030acdf86df/353/569/90/False/the-hobbit-3.jpg",
+          previewText: "An unexpected party"
+        },
+        {
+          id: "2",
+          title: "The Lord of the Rings",
+          thumbnail:
+            "https://kbimages1-a.akamaihd.net/92525c4a-613e-492d-ad9f-44d646c14d8c/353/569/90/False/the-fellowship-of-the-ring.jpg",
+          previewText: "A long expected party"
+        },
+        {
+          id: "3",
+          title: "The Silmarillion",
+          thumbnail:
+            "https://kbimages1-a.akamaihd.net/af1f059d-c5db-496b-9650-ad702bb389af/353/569/90/False/the-silmarillion-1.jpg",
+          previewText: "The Valaquenta"
+        },
+        {
+          id: "4",
+          title: "Unfinished Tales",
+          thumbnail: "",
+          previewText: "of Numenor and Middle-earth"
+        }
+      ]
+    };
+  },
   components: {
     PostList
   }
@@ -23,7 +56,7 @@ export default {
   position: relative;
   padding: 30px;
   box-sizing: border-box;
-  background-image: url('~assets/img/main-page-background.jpg');
+  background-image: url("~assets/img/main-page-background.jpg");
   background-position: center;
   background-size: cover;
 }
