5dc0d0735ca50f17344367bd68f48969ffc201ab
stefano-b <bartoletti.stefano@gmail.com>
2020-06-15 18:14:52 +0200

    store data in Firebase

--- module-3/components/Admin/AdminPostForm.vue
+++ module-3/components/Admin/AdminPostForm.vue
@@ -42,7 +42,7 @@ export default {
   methods: {
     onSave() {
       // Save the post
-      console.log(this.editedPost);
+      this.$emit('submit', this.editedPost);
     },
     onCancel() {
       // Navigate back
--- module-3/pages/admin/new-post/index.vue
+++ module-3/pages/admin/new-post/index.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="admin-new-post-page">
     <section class="new-post-form">
-      <admin-post-form></admin-post-form>
+      <admin-post-form @submit="onSubmitted"></admin-post-form>
     </section>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import AdminPostForm from '@/components/Admin/AdminPostForm';
 
 export default {
   components: {
     AdminPostForm
+  },
+  methods: {
+    onSubmitted(postData) {
+      axios.post('https://sb-nuxt-blog.firebaseio.com/posts.json', postData)
+      .then(result => console.log(result))
+      .catch(e => console.log(e))
+    }
   },
   layout: 'admin'
 };
