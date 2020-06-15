816c3fa1db0416adcbd471a11adb2d45034eb3ed
stefano-b <bartoletti.stefano@gmail.com>
2020-06-15 19:08:33 +0200

    set correct field names

--- module-3/components/Admin/AdminPostForm.vue
+++ module-3/components/Admin/AdminPostForm.vue
@@ -2,8 +2,9 @@
   <form @submit.prevent="onSave">
     <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
     <AppControlInput v-model="editedPost.title">Title</AppControlInput>
-    <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
+    <AppControlInput v-model="editedPost.thumbnail">Thumbnail Link</AppControlInput>
     <AppControlInput control-type="textarea" v-model="editedPost.content">Content</AppControlInput>
+    <AppControlInput control-type="textarea" v-model="editedPost.previewText">Preview Text</AppControlInput>
 
     <AppButton type="submit">Save</AppButton>
     <AppButton type="button" style="margin-left: 10px" btn-style="cancel" @click="onCancel">Cancel</AppButton>
@@ -24,8 +25,9 @@ export default {
         : {
             author: '',
             title: '',
-            thumbnailLink: '',
-            content: ''
+            thumbnail: '',
+            content: '',
+            previewText: ''
           }
     };
   },
