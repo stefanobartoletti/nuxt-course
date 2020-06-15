2a11db7f381f1eed3430bce9eae318f0d86822cd
stefano-b <bartoletti.stefano@gmail.com>
2020-06-15 19:23:36 +0200

    fetch single post

--- module-3/pages/admin/new-post/index.vue
+++ module-3/pages/admin/new-post/index.vue
@@ -17,7 +17,10 @@ export default {
   methods: {
     onSubmitted(postData) {
       axios
-        .post('https://sb-nuxt-blog.firebaseio.com/posts.json', postData)
+        .post('https://sb-nuxt-blog.firebaseio.com/posts.json', {
+          ...postData,
+          updatedDate: new Date()
+        })
         .then(result => console.log(result))
         .catch(e => console.log(e));
     }
